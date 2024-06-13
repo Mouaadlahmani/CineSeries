1812323e3a8b7716a640802436f2fad8fb604503
Mouaadlahmani <157011355+Mouaadlahmani@users.noreply.github.com>
2024-06-13 16:12:29 +0100

    Creation de model class user

---
+++ CineSeries/src/test/java/com/Mouad/CineBase/CineBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Mouad.CineBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
