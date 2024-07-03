648d25cbbb6e5a985a4c4305c389cb47cf3e425f
Mouaadlahmani <157011355+Mouaadlahmani@users.noreply.github.com>
2024-07-03 22:49:55 +0100

    Delete CineSeries directory

--- CineSeries/src/main/java/com/Mouad/CineBase/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Mouad.CineBase.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.Mouad.CineBase.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long>{
-
-}
